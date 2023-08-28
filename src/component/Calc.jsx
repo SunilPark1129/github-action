0a090acad4d0c5b1546936106a963c5622a362cd
Sunil Park <sunilpark1129@gmail.com>
2023-08-27 23:21:31 -0400

    Added test

---
+++ src/component/Calc.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function Calc() {
+  const [multTwo, setMultTwo] = useState(0);
+  const [inputVal, setInputVal] = useState(0);
+
+  function clickHandler() {
+    setMultTwo(inputVal * 2);
+  }
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="input-val">Input:</label>
+        <input
+          type="text"
+          id="input-val"
+          placeholder="type input..."
+          onChange={(e) => setInputVal(e.target.value)}
+          value={inputVal}
+          data-testid="calc-input"
+        />
+        <button onClick={clickHandler} data-testid="calc-btn">
+          submit
+        </button>
+      </div>
+      <div>
+        <p data-testid="calc-output-val">Input: {inputVal}</p>
+        <p data-testid="calc-output-mult-val">Calculated: {multTwo}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/component/__test__/Calc.test.js
@@ -0,0 +1,60 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Calc from "../Calc";
+import userEvent from "@testing-library/user-event";
+
+describe("Testing the multiply numbers...", () => {
+  test("check if elements are existing", () => {
+    render(<Calc />);
+    const inputEl = screen.getByPlaceholderText(/type input/);
+    const btnEl = screen.getByRole("button", { name: /submit/ });
+    const outputText = screen.getByTestId("calc-output-val");
+    const outputMultipledText = screen.getByTestId("calc-output-mult-val");
+
+    expect(inputEl).toBeInTheDocument();
+    expect(btnEl).toBeInTheDocument();
+    expect(outputText).toBeInTheDocument();
+    expect(outputMultipledText).toBeInTheDocument();
+  });
+  test("if input has value, then p tag changes its inner text", async () => {
+    render(<Calc />);
+    const inputEl = screen.getByPlaceholderText(/type input/);
+    const btnEl = screen.getByRole("button", { name: /submit/ });
+    const outputText = screen.getByTestId("calc-output-val");
+    const outputMultipledText = screen.getByTestId("calc-output-mult-val");
+
+    fireEvent.change(inputEl, {
+      target: {
+        value: 2,
+      },
+    });
+    fireEvent.click(btnEl);
+    expect(outputText).toHaveTextContent("2");
+    expect(outputMultipledText).toHaveTextContent("4");
+
+    fireEvent.change(inputEl, {
+      target: {
+        value: 4,
+      },
+    });
+    fireEvent.click(btnEl);
+    expect(outputText).toHaveTextContent("4");
+    expect(outputMultipledText).toHaveTextContent("8");
+  });
+
+  test("if output gets wrong value", () => {
+    render(<Calc />);
+    const inputEl = screen.getByPlaceholderText(/type input/);
+    const btnEl = screen.getByRole("button", { name: /submit/ });
+    const outputText = screen.getByTestId("calc-output-val");
+    const outputMultipledText = screen.getByTestId("calc-output-mult-val");
+
+    fireEvent.change(inputEl, {
+      target: {
+        value: 3,
+      },
+    });
+    fireEvent.click(btnEl);
+    expect(outputText).toHaveTextContent("3");
+    expect(outputMultipledText).not.toHaveTextContent("9");
+  });
+});
